c052070ac9a864f71537921ec90ac794cb75b6b1
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-12 21:06:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_1_ex_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            double D;
+
+
+            Console.Write("Digite o diametro do circulo: ");
+
+            D = double.Parse(Console.ReadLine());
+
+            A = 3.14 * D;
+
+            Console.WriteLine("AREA DO CIRCULO É: "+ A);
+        }
+    }
+}
